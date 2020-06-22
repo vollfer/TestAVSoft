df47719b227cc7102c228d5dba2bc133b0d3c85a
Александр Бровков <volfer2011@gmail.com>
2020-06-22 18:31:59 +0300

    no message

---
+++ TestAVSoft/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAVSoft
+//
+//  Created by Александр  Бровков  on 22.06.2020.
+//  Copyright © 2020 Александр  Бровков . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
